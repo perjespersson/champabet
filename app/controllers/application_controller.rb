25159125311fd9602583c2da0bd80ce8c9e6717d
Jesper Persson <jesper.persson_96@hotmail.com>
2022-05-05 18:18:44 +0200

    removed modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource)
+    puts "-------"
+    puts resource
+    puts "-------"
+    root_path
+  end
 end
