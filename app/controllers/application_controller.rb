cb090da45d2cc40b7e4215055ee092e80b5b1ece
Jesper Persson <jesper.persson_96@hotmail.com>
2022-05-06 11:01:09 +0200

    added username to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-  private
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource)
-    puts "-------"
-    puts resource
-    puts "-------"
-    root_path
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    #devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
