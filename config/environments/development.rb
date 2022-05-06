e43db21854a9981546c328b6c1ebc90ac21e2d72
Jesper Persson <jesper.persson_96@hotmail.com>
2022-05-06 17:25:44 +0200

    form design

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1028 }
+  config.action_mailer.perform_deliveries = true
 end
